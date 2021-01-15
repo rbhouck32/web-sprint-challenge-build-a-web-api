d9e43c4909aff04baca738d4bf0349b5a15607f6
Rob <rbhouck32@gmail.com>
2021-01-15 13:35:50 -0500

    this is the completion of my validation middleware

--- api/middleware/index.js
+++ api/middleware/index.js
@@ -17,6 +17,22 @@ async function validateActionsId(req, res, next) {
   }
 }
 
+async function validateProjectsId(req, res, next) {
+  try {
+    const project = await Projects.get(req.params.id);
+    if (project) {
+      req.project = project;
+      next();
+    } else {
+      res
+        .status(404)
+        .json({ message: `Project containing id ${req.params.id} not found` });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Bad Request" });
+  }
+}
+
 function validateAction(req, res, next) {
   const body = req.body;
   const project_id = req.body.project_id;
@@ -27,12 +43,30 @@ function validateAction(req, res, next) {
     if (project_id && description && notes) {
       next();
     } else {
-      res
-        .status(400)
-        .json({
-          message: `One of the required fields is missing please check your request again`,
-        });
+      res.status(400).json({
+        message: `One of the required fields is missing please check your request again`,
+      });
     }
+  } else {
+    res.status(400).json({ message: "Missing Action data" });
+  }
+}
+
+function validateProject(req, res, next) {
+  const body = req.body;
+  const name = req.body.name;
+  const description = req.body.description;
+
+  if (body) {
+    if (name && description) {
+      next();
+    } else {
+      res.status(400).json({
+        message: `missing required fields. Please check the name and description fields again.`,
+      });
+    }
+  } else {
+    res.status(400).json({ message: "missing user data" });
   }
 }
 
